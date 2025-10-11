b7a5b8801cb488bb40784f3c8eaa6b407106395b
Jatan Bhatt <jatanvishal30@gmail.com>
2025-10-12 03:22:10 +0530

    dashboard footer part added

--- Backend/routes/skillsRoutes.js
+++ Backend/routes/skillsRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/authMiddleware");
-const { createSkill, listMySkills } = require("../controllers/skillsController");
+const { createSkill, listMySkills, getSkillsByCategory, getSkillTeachers } = require("../controllers/skillsController");
 
 router.post("/", protect, createSkill);
 router.get("/mine", protect, listMySkills);
+router.get("/category/:category", getSkillsByCategory);
+router.get("/teachers/:skillName", getSkillTeachers);
 
 module.exports = router;
 
